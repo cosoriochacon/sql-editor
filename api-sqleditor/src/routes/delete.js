797a8e968d26e16d27d91e4d42243facf7d1a2a4
ccdosorio <ccdosorio11@gmail.com>
2021-10-13 02:02:10 -0600

    Delete local backend finalizado

--- api-sqleditor/src/routes/delete.js
+++ api-sqleditor/src/routes/delete.js
@@ -1,45 +1,5 @@
-const fs = require("fs");
-const ParserModel = require("../models/parserModel");
+const DeleteController = require("../controllers/delete.controller");
 
 module.exports = (app) => {
-  app.post("/api/query/delete", async (req, res) => {
-    let { query } = req.body;
-    const statements = await ParserModel.queryParser(query);
-    let from = statements.from.name;
-    let nameTable = from.split(".")[1].trim();
-    let nameDB = from.split(".")[0].trim();
-    let nameFile = nameTable + ".json";
-    let path = "files/" + nameFile;
-    let file = fs.existsSync(path);
-    if (file) {
-      fs.readFile(path, (err, data) => {
-        if (err) {
-          res.json({ status: 0, msg: "Error" });
-        } else {
-          obj = JSON.parse(data);
-          let pos_where = obj.columns.indexOf(statements.where[0].left.name);
-          let pos_data;
-          for (let i = 0; i < obj.data.length; i++) {
-            if (obj.data[i][pos_where] === statements.where[0].right.name) {
-              pos_data = i;
-            }
-          }
-          let dataTemp = obj.data[pos_data];
-
-          for (let i = 0; i < dataTemp.length; i++) {
-            if (dataTemp[i] === statements.where[0].right.name) {
-              obj.data.splice(pos_data, 1);
-            }
-          }
-
-          let json = JSON.stringify(obj);
-          fs.writeFileSync(path, json, "utf-8");
-          res.json({
-            status: 1,
-            msg: `Delete table ${nameTable} on server ${nameDB} successfull`,
-          });
-        }
-      });
-    }
-  });
+  app.post("/api/query/delete", DeleteController.deleteQuery);
 };
