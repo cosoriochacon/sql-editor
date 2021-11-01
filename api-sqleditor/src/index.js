f6fe50eed38820e58316576947372537b4c62327
ccdosorio <ccdosorio11@gmail.com>
2021-10-31 23:46:43 -0600

    Cambios en las rutas

--- api-sqleditor/src/index.js
+++ api-sqleditor/src/index.js
@@ -1,9 +1,10 @@
 const app = require("./config/server");
 
-// Rutas
+/**
+ * Rutas
+ */
 require("./routes/user")(app);
 require("./routes/parser")(app);
-require("./routes/schemas")(app);
 
 app.listen(app.get("port"), () => {
   console.log(`Server running port ${app.get("port")}`);
